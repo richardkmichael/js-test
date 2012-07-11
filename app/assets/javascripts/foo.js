927455b328648e56eb3a62ee7156b9f726b82ba1
Richard Michael <rmichael@edgeofthenet.org>
2012-07-11 20:16:45 +0200

    JS is running twice, be sure the first timer reference is not lost.

--- app/assets/javascripts/foo.js
+++ app/assets/javascripts/foo.js
@@ -1,6 +1,23 @@
-var readyStateCheckInterval = setInterval(function () {
-  if (document.readyState === 'complete') {
-    console.log('Document ready.');
-    clearInterval(readyStateCheckInterval);
+/*jslint indent: 2 */
+
+(function () {
+
+  'use strict';
+
+  var intervalTimers = {},
+    date = new Date(),
+    time = date.getTime();
+
+  function setupIntervalTimer(name) {
+
+    intervalTimers[name] = setInterval(function () {
+      if (document.readyState === 'complete') {
+        console.log('Document ready.');
+        clearInterval(intervalTimers[name]);
+      }
+    }, 2000);
   }
-}, 2000);
+
+  setupIntervalTimer(time);
+
+}());
